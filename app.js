924632e9d7eb8c0a5d63d55a9937dff1a1611887
jasminabasurita <jasminejacquelin@gmail.com>
2017-11-07 11:49:43 -0500

    created router.js and imported various styling and html templates

--- app.js
+++ app.js
@@ -3,43 +3,48 @@ const app = express();
 
 const nunjucks = require('nunjucks');
 
+const routes = require('./routes');
+
 
 app.listen(3000, function(){
   console.log('Twitter up and running!')
 })
-
 app.use(function(req, res, next){
   console.log(req.method, req.path);
   next();
 })
-
-app.get('/', function(req, res, next){
-  res.send('Hello World!')
-})
-
-app.get('/news', function(req, res, next){
-  res.send("Here's the news!")
-})
-
-app.get('/views', function(req, res, next){
-  res.render('index', locals, function (err, output) {
-    if (err) throw err;
-    res.send(output);
-    console.log(output);
-  });
-});
-
-var locals = {
-  title: 'nunjucks will make our lives easier eventually',
-  people: [
-    {name: 'For real'},
-    {name: "Even if it sucks at the moment"}
-  ]
-};
+app.use(routes);
 
 app.set('view engine', 'html')
 app.engine('html', nunjucks.render);
 nunjucks.configure('views', {noCache: true});
+
+
 // nunjucks.render('index.html', locals, function (err, output) {
 //   console.log(output);
 // })
+
+// app.get('/', function(req, res, next){
+//   res.send('Hello World!')
+// })
+
+// app.get('/news', function(req, res, next){
+//   res.send("Here's the news!")
+// })
+
+// app.get('/views', function(req, res, next){
+//   res.render('index', locals, function (err, output) {
+//     if (err) throw err;
+//     res.send(output);
+//     console.log(output);
+//   });
+// });
+
+
+// var locals = {
+//   title: 'nunjucks will make our lives easier eventually',
+//   people: [
+//     {name: 'For real'},
+//     {name: "Even if it sucks at the moment"}
+//   ]
+// };
